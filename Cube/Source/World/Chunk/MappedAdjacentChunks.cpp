5092b190d31748448383ecbdf8fcf8be7b3aac50
gabkhanfig <gabkhanfig@gmail.com>
2023-07-16 23:42:00 -0400

    Support For Buried Bitmask

--- Cube/Source/World/Chunk/MappedAdjacentChunks.cpp
+++ Cube/Source/World/Chunk/MappedAdjacentChunks.cpp
@@ -1,6 +1,16 @@
 #include "MappedAdjacentChunks.h"
 #include "../World.h"
 #include "Chunk.h"
+#include "ChunkRenderComponent.h"
+
+static int GetMappedAdjacentOffsetIndex(const ChunkPosition position, const ChunkPosition startPos) {
+	gk_assertm(position.x >= startPos.x && position.x < startPos.x + 3, "Position x value is out of range of +0 -> +2 of starting position x\nposition x: " << position.x << "\nstart position x: " << startPos.x);
+	gk_assertm(position.y >= startPos.y && position.y < startPos.y + 3, "Position y value is out of range of +0 -> +2 of starting position x\nposition y: " << position.y << "\nstart position y: " << startPos.y);
+	gk_assertm(position.z >= startPos.z && position.z < startPos.z + 3, "Position z value is out of range of +0 -> +2 of starting position x\nposition z: " << position.z << "\nstart position z: " << startPos.z);
+	ChunkPosition offset = ChunkPosition(position.x - startPos.x, position.y - startPos.y, position.z - startPos.z);
+	const int index = offset.x * 9 + offset.y * 3 + offset.z;
+	return index;
+}
 
 const MappedAdjacentChunks MappedAdjacentChunks::Create(const World* world, const ChunkPosition centerChunkPos)
 {
@@ -24,12 +34,24 @@ const MappedAdjacentChunks MappedAdjacentChunks::Create(const World* world, cons
 
 const Chunk* MappedAdjacentChunks::GetChunk(const ChunkPosition position) const
 {
-	gk_assertm(position.x >= _startPos.x && position.x < _startPos.x + 3, "Position x value is out of range of +0 -> +2 of starting position x\nposition x: " << position.x << "\nstart position x: " << _startPos.x);
-	gk_assertm(position.y >= _startPos.y && position.y < _startPos.y + 3, "Position y value is out of range of +0 -> +2 of starting position x\nposition y: " << position.y << "\nstart position y: " << _startPos.y);
-	gk_assertm(position.z >= _startPos.z && position.z < _startPos.z + 3, "Position z value is out of range of +0 -> +2 of starting position x\nposition z: " << position.z << "\nstart position z: " << _startPos.z);
-	ChunkPosition offset = ChunkPosition(position.x - _startPos.x, position.y - _startPos.y, position.z - _startPos.z);
-	const int index = offset.x + offset.y * 3 + offset.z * 9;
+	const int index = GetMappedAdjacentOffsetIndex(position, _startPos);
 	const Chunk* chunk = _chunks[index];
+	if (chunk == nullptr) return nullptr;
+	//if (chunk->GetPosition() != position) {
+	//	std::cout << "_startPos: " << String::From(_startPos) << std::endl;
+	//	std::cout << "arg chunk position: " << String::From(position) << std::endl;
+	//	std::cout << "index: " << index << std::endl;
+	//	std::cout << "found chunk position: " << String::From(chunk->GetPosition()) << std::endl;
+	//	for (int i = 0; i < 27; i++) {
+	//		if (_chunks[i] == nullptr) continue;
+	//		if (_chunks[i]->GetPosition() == position) {
+	//			std::cout << "the correct index should be: " << i << std::endl;
+	//			break;
+	//		}
+	//	}
+	//	//std::cout << "Chunk position does not match the position of the function argument\nchunk position: " << String::From(chunk->GetPosition()) << "\narg position: " << String::From(position) << std::endl;
+	//	DebugBreak();
+	//}
 	gk_assertm(chunk->GetPosition() == position,
 		"Chunk position does not match the position of the function argument\nchunk position: " << String::From(chunk->GetPosition()) << "\narg position: " << String::From(position));
 	return chunk;
@@ -37,5 +59,56 @@ const Chunk* MappedAdjacentChunks::GetChunk(const ChunkPosition position) const
 
 const Block* MappedAdjacentChunks::GetBlock(const WorldPosition position) const
 {
+	const Chunk* chunk = GetChunk(position.ToChunkPosition());
+	return chunk == nullptr ? nullptr : chunk->GetBlock(position.ToBlockPosition());
+}
+
+const MappedAdjacentAndBuriedChunks MappedAdjacentAndBuriedChunks::Create(const World* world, const ChunkPosition centerChunkPos)
+{
+	gk_assertNotNull(world);
+	MappedAdjacentAndBuriedChunks adjacent;
+	const ChunkPosition startPos = ChunkPosition(centerChunkPos.x - 1, centerChunkPos.y - 1, centerChunkPos.z - 1);
+	adjacent._startPos = startPos;
+
+	int index = 0;
+	for (int x = 0; x < 3; x++) {
+		for (int y = 0; y < 3; y++) {
+			for (int z = 0; z < 3; z++) {
+				Chunk* chunk = world->GetChunk(ChunkPosition(startPos.x + x, startPos.y + y, startPos.z + z));
+				if (chunk == nullptr) {
+					adjacent._chunks[index] = nullptr; 
+					adjacent._buriedBitmasks[index] = BuriedChunkBlocks();
+					index++;
+					continue;
+				}
+				adjacent._chunks[index] = world->GetChunk(ChunkPosition(startPos.x + x, startPos.y + y, startPos.z + z));
+				adjacent._buriedBitmasks[index] = chunk->GetRenderComponent()->GetBuriedBlocksBitmask();
+				index++;
+			}
+		}
+	}
+
+	return adjacent;
+}
+
+const Chunk* MappedAdjacentAndBuriedChunks::GetChunk(const ChunkPosition position) const
+{
+	const int index = GetMappedAdjacentOffsetIndex(position, _startPos);
+	const Chunk* chunk = _chunks[index];
+	gk_assertm(chunk->GetPosition() == position,
+		"Chunk position does not match the position of the function argument\nchunk position: " << String::From(chunk->GetPosition()) << "\narg position: " << String::From(position));
+	return chunk;
+}
+
+const Block* MappedAdjacentAndBuriedChunks::GetBlock(const WorldPosition position) const
+{
 	return GetChunk(position.ToChunkPosition())->GetBlock(position.ToBlockPosition());
+}
+
+const bool MappedAdjacentAndBuriedChunks::IsBlockBuried(const WorldPosition position) const
+{
+	const ChunkPosition chunkPos = position.ToChunkPosition();
+	const int chunkIndex = GetMappedAdjacentOffsetIndex(chunkPos, _startPos);
+	return _buriedBitmasks[chunkIndex].IsBlockBuried(position.ToBlockPosition());
+
 }
--- Cube/Source/World/Chunk/MappedAdjacentChunks.h
+++ Cube/Source/World/Chunk/MappedAdjacentChunks.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "../WorldTransform.h"
+#include "BuriedChunkBlocks.h"
 
 class World;
 class Chunk;
 class Block;
 
-struct MappedAdjacentChunks {
-
+struct MappedAdjacentChunks 
+{
 	static const MappedAdjacentChunks Create(const World* world, const ChunkPosition centerChunkPos);
 
 	const Chunk* GetChunk(const ChunkPosition position) const;
@@ -19,5 +20,26 @@ private:
 	const Chunk* _chunks[27];
 	// Bottom left
 	ChunkPosition _startPos;
+
+};
+
+struct MappedAdjacentAndBuriedChunks 
+{
+	static const MappedAdjacentAndBuriedChunks Create(const World* world, const ChunkPosition centerChunkPos);
+
+	const Chunk* GetChunk(const ChunkPosition position) const;
+
+	const Block* GetBlock(const WorldPosition position) const;
+
+	const bool IsBlockBuried(const WorldPosition position) const;
+
+private:
+
+	BuriedChunkBlocks _buriedBitmasks[27];
+
+	const Chunk* _chunks[27];
+	// Bottom left
+	ChunkPosition _startPos;
+
 
 };
