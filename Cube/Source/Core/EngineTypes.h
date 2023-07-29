0fa5c9782b09b8678bc030903ccf983c19ba0742
gabkhanfig <gabkhanfig@gmail.com>
2023-07-28 22:31:41 -0400

    Check For Floats Nearly Equal

--- Cube/Source/Core/EngineTypes.h
+++ Cube/Source/Core/EngineTypes.h
@@ -29,6 +29,20 @@ using HashSet = std::unordered_set<T>;
 #define forceinline __forceinline
 
 template<>
+[[nodiscard]] inline static gk::String gk::String::From<glm::vec3>(const glm::vec3& value) {
+	return gk::String("x: ") + gk::String::FromFloat(value.x) + gk::String(", y: ") + gk::String::FromFloat(value.y) + gk::String(", z: ") + gk::String::FromFloat(value.z);
+}
+
+template<>
 [[nodiscard]] inline static gk::String gk::String::From<glm::dvec3>(const glm::dvec3& value) {
 	return gk::String("x: " ) + gk::String::FromFloat(value.x) + gk::String(", y: ") + gk::String::FromFloat(value.y) + gk::String(", z: ") + gk::String::FromFloat(value.z);
 }
+
+inline bool IsNearlyEqual(double a, double b, double errorTolerance = (1.e-8)) {
+	return std::abs(a - b) <= errorTolerance;
+}
+
+inline bool IsNearlyEqual(float a, float b, float errorTolerance = (1.e-8)) {
+	return std::abs(a - b) <= errorTolerance;
+}
+
