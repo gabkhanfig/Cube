27089d68eaac377fe5d1b4fb25a8eed45de73634
gabkhanfig <gabkhanfig@gmail.com>
2023-07-30 17:02:22 -0400

    Added Extra Test Checks

--- Cube/Testing/UnitTests/Geometry/PackedTypes/PackedBlockOffsetPositionTests.cpp
+++ Cube/Testing/UnitTests/Geometry/PackedTypes/PackedBlockOffsetPositionTests.cpp
@@ -53,7 +53,8 @@ namespace UnitTests
 		for (int i = (-1 * CHUNK_LENGTH); i <= (CHUNK_LENGTH * 2); i++) { // -8 -> 16 inclusive
 			const glm::vec3 pos{i, 0, 0};
 			PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
-			EXPECT_EQ(packed.Unpack().x, pos.x);
+			EXPECT_EQ(packed.Unpack().x, pos.x); 
+			EXPECT_EQ(packed.Unpack(), pos);
 		}
 	}
 
@@ -62,6 +63,7 @@ namespace UnitTests
 			const glm::vec3 pos{0, i, 0};
 			PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
 			EXPECT_EQ(packed.Unpack().y, pos.y);
+			EXPECT_EQ(packed.Unpack(), pos);
 		}
 	}
 
@@ -70,6 +72,7 @@ namespace UnitTests
 			const glm::vec3 pos{0, 0, i};
 			PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
 			EXPECT_EQ(packed.Unpack().z, pos.z);
+			EXPECT_EQ(packed.Unpack(), pos);
 		}
 	}
 
@@ -77,6 +80,7 @@ namespace UnitTests
 		const glm::vec3 pos{1.f / 16.f, 0, 0};
 		PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
 		EXPECT_EQ(packed.Unpack().x, 1.f / 16.f);
+		EXPECT_EQ(packed.Unpack(), pos);
 	}
 
 	TEST(PackedBlockOffsetPositionTest, RoundTo16th) {
@@ -89,12 +93,14 @@ namespace UnitTests
 		const glm::vec3 pos{(1.f / 16.f) - 8, 0, 0};
 		PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
 		EXPECT_EQ(packed.Unpack().x, (1.f / 16.f) - 8);
+		EXPECT_EQ(packed.Unpack(), pos);
 	}
 
 	TEST(PackedBlockOffsetPositionTest, Lossless16thPositiveExtension) {
 		const glm::vec3 pos{(1.f / 16.f) + 8, 0, 0};
 		PackedBlockOffsetPosition packed = PackedBlockOffsetPosition::Pack(pos);
 		EXPECT_EQ(packed.Unpack().x, (1.f / 16.f) + 8);
+		EXPECT_EQ(packed.Unpack(), pos);
 	}
 }
 
