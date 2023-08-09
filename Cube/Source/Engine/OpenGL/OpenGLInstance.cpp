bdd295ef0d58bf84ee2f300847a72c5c3491c046
gabkhanfig <gabkhanfig@gmail.com>
2023-08-09 18:34:12 -0400

    Open GL Instance

---
+++ Cube/Source/Engine/OpenGL/OpenGLInstance.cpp
@@ -0,0 +1,49 @@
+#include "OpenGLInstance.h"
+#include <glad/glad.h>
+#include "../Engine.h"
+
+static void GLAPIENTRY DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+		(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+		type, severity, message);
+}
+
+OpenGLInstance::OpenGLInstance(gk::Thread* renderThread) : 
+	renderThreadId(renderThread != nullptr ? renderThread->StdThreadId() : std::this_thread::get_id()),
+	isOpenGLInitialized(false)
+{}
+
+bool OpenGLInstance::IsExecutingOnRenderThread() const
+{
+	return std::this_thread::get_id() == renderThreadId;
+}
+
+void OpenGLInstance::InitializeOpenGL(gk::Thread* renderThread, const glm::ivec2 viewportSize, const glm::vec3 clearColor)
+{
+	if (renderThread == nullptr) {
+		LoadAndInitializeOpenGL(viewportSize, clearColor);
+	}
+	else {
+		renderThread->BindFunction(std::bind(OpenGLInstance::LoadAndInitializeOpenGL, viewportSize, clearColor));
+		renderThread->Execute();
+		while(!renderThread->IsReady());
+	}
+
+	isOpenGLInitialized = true;
+}
+
+void OpenGLInstance::LoadAndInitializeOpenGL(const glm::ivec2 viewportSize, const glm::vec3 clearColor)
+{
+	assertOnRenderThread();
+	gladLoadGL();
+	glViewport(0, 0, viewportSize.x, viewportSize.y);
+	glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.f);
+	glEnable(GL_DEPTH_TEST | GL_DEBUG_OUTPUT);
+	glDepthFunc(GL_LESS);
+	glDepthMask(GL_TRUE);
+	glDebugMessageCallback(DebugMessageCallback, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CW); // See chunk mesh index buffer object. this works and i dont know why
+}
---
+++ Cube/Source/Engine/OpenGL/OpenGLInstance.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../EngineCore.h"
+
+#define assertOnRenderThread() gk_assertm(engine->GetOpenGLInstance()->IsExecutingOnRenderThread(), "Cannot call OpenGL functions on any thread except for the rendering thread")
+
+class OpenGLInstance
+{
+public:
+	/* Passing in nullptr means no render thread. */
+	OpenGLInstance(gk::Thread* renderThread);
+
+	bool IsExecutingOnRenderThread() const;
+
+	/* Passing in nullptr means no render thread. */
+	void InitializeOpenGL(gk::Thread* renderThread, const glm::ivec2 viewportSize, const glm::vec3 clearColor);
+
+private:
+
+	static void LoadAndInitializeOpenGL(const glm::ivec2 viewportSize, const glm::vec3 clearColor);
+
+private:
+
+	const std::thread::id renderThreadId;
+	bool isOpenGLInitialized;
+};
