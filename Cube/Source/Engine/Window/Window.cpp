7527a1bd5df1b32cc13205f2770011a3ffdc552b
gabkhanfig <gabkhanfig@gmail.com>
2023-08-09 01:17:43 -0400

    GLFW Window Hints

--- Cube/Source/Engine/Window/Window.cpp
+++ Cube/Source/Engine/Window/Window.cpp
@@ -12,6 +12,11 @@ void Window::InitializeGLFW()
 	if (!glfwInit())
 		abort();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
 	isGlfwInitialized = true;
 	//glfwWindowHint(GLFW_DEPTH_BITS, 24);
 }
