48f5dbc305f1642f52e00e085c61997b4fbc0419
gabkhanfig <gabkhanfig@gmail.com>
2023-08-17 23:19:27 -0400

    Persistent Mapped Triple IBO Specialization

---
+++ Cube/Source/Graphics/OpenGL/Buffers/MappedTripleIbo.cpp
@@ -0,0 +1,47 @@
+#include "MappedTripleIbo.h"
+#include "IndexBufferObject.h"
+#include "../../../Engine/OpenGL/OpenGLInstance.h"
+#include "../../../Engine/Engine.h"
+
+MappedTripleIbo::MappedTripleIbo() : capacity(0), boundId(0), modifyId(1) {}
+
+MappedTripleIbo::~MappedTripleIbo()
+{
+	assertOnRenderThread();
+	DeleteBuffers();
+}
+
+void MappedTripleIbo::Reserve(uint32 count)
+{
+	assertOnRenderThread();
+	gk_assertm(count != 0, "Cannot reserve mapped triple index buffer objects with a capacity of 0");
+
+	DeleteBuffers();
+	capacity = count;
+	boundId = 0;
+	modifyId = 1;
+
+	for (int i = 0; i < 3; i++) {
+		Mapped mapped;
+		mapped.buffer = new IndexBufferObject();
+		mapped.data = mapped.buffer->CreatePersistentMappedStorage(capacity);
+		mappedBuffers[i] = mapped;
+	}
+}
+
+void MappedTripleIbo::SwapNextBuffer()
+{
+	assertOnRenderThread();
+	boundId = (boundId + 1) % 3;
+	modifyId = (modifyId + 1) % 3;
+}
+
+void MappedTripleIbo::DeleteBuffers()
+{
+	assertOnRenderThread();
+	for (int i = 0; i < 3; i++) {
+		IndexBufferObject* bufferObject = mappedBuffers[i].buffer;
+		if (bufferObject) delete bufferObject;
+		mappedBuffers[i] = Mapped();
+	}
+}
---
+++ Cube/Source/Graphics/OpenGL/Buffers/MappedTripleIbo.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "../../../Engine/EngineCore.h"
+
+class IndexBufferObject;
+
+class MappedTripleIbo
+{
+public:
+
+	struct Mapped {
+		IndexBufferObject* buffer;
+		uint32* data;
+
+		Mapped() : buffer(nullptr), data(nullptr) {}
+	};
+
+	MappedTripleIbo();
+
+	~MappedTripleIbo();
+
+	void Reserve(uint32 count);
+
+	void SwapNextBuffer();
+
+	Mapped& GetBoundMapped() { return mappedBuffers[boundId]; }
+	Mapped& GetModifyMapped() { return mappedBuffers[modifyId]; }
+	uint32 GetCapacity() const { return capacity; }
+
+	IndexBufferObject* GetBoundBuffer() const { return mappedBuffers[boundId].buffer; }
+
+private:
+
+	void DeleteBuffers();
+
+private:
+
+	Mapped mappedBuffers[3];
+	uint32 capacity;
+	uint32 boundId;
+	uint32 modifyId;
+};
