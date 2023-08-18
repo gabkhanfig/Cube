b3e7a9674a7fefac3723f3875ed96aa0a60a0147
gabkhanfig <gabkhanfig@gmail.com>
2023-08-18 17:03:36 -0400

    Fixed Name

--- Cube/Source/Graphics/OpenGL/OpenGLEnums.cpp
+++ Cube/Source/Graphics/OpenGL/OpenGLEnums.cpp
@@ -1,6 +1,6 @@
 #include "OpenGLEnums.h"
 #include <glad/glad.h>
 
-static_assert(static_cast<int>(GLBufferAccess::ReadOnly) == GL_READ_ONLY);
-static_assert(static_cast<int>(GLBufferAccess::WriteOnly) == GL_WRITE_ONLY);
-static_assert(static_cast<int>(GLBufferAccess::ReadWrite) == GL_READ_WRITE);
+static_assert(static_cast<int>(GLMappedBufferAccess::ReadOnly) == GL_READ_ONLY);
+static_assert(static_cast<int>(GLMappedBufferAccess::WriteOnly) == GL_WRITE_ONLY);
+static_assert(static_cast<int>(GLMappedBufferAccess::ReadWrite) == GL_READ_WRITE);
