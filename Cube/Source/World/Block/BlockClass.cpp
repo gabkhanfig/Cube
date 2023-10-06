4c83ffab8ce5ee98830cc6fefe272e13926c9627
gabkhanfig <gabkhanfig@gmail.com>
2023-10-06 00:36:41 -0400

    Implementation of new Block RTTI

--- Cube/Source/World/Block/BlockClass.cpp
+++ Cube/Source/World/Block/BlockClass.cpp
@@ -1,65 +1,22 @@
 #include "BlockClass.h"
+#include "BlockTypeInfo.h"
 
-const BlockVTable* IBlockClass::NewBlockVTable() const
-{
-	BlockVTable* vTable = BlockVTable::Default(this->GetBlockName());
-	this->ConstructBlockVTable(vTable);
-	return vTable;
-}
-
-Block AirBlockClass::ClassDefaultBlock() const
-{
-	Block air;
-	air.isFullSolid = false;
-	return air;
-}
-
-void AirBlockClass::ConstructBlockVTable(BlockVTable* vTable) const
-{
-	vTable->meshFunc = AirBlockClass::AirNoMesh;
-	vTable->getFaceTexture = AirBlockClass::GetFaceTexture;
-}
-
-BlockMaterial AirBlockClass::GetMaterial() const
-{
-	gk_assertm(false, "Air Block does not have a material");
-	return BlockMaterial();
-}
-
-EBlockTexture AirBlockClass::GetFaceTexture(const Block* self, const BlockFacing face)
+EBlockTexture AirBlockClass::getFaceTexture(const Block* self, const BlockFacing face)
 {
 	return EBlockTexture::none;
 }
 
-void AirBlockClass::AirNoMesh(const Block* self, ChunkMesh* mesh, const Chunk* chunk, const WorldPosition position, const glm::vec3 vertexOffset, const MappedAdjacentAndBuriedChunks& adjacentChunks)
+void AirBlockClass::constructMesh(const Block* self, const BlockVTable* vTable, ChunkMesh* mesh, const Chunk* chunk, const WorldPosition& position, const glm::vec3& vertexOffset, const MappedAdjacentAndBuriedChunks& adjacentChunks)
 {
 	return;
 }
 
-Block StoneBlockClass::ClassDefaultBlock() const
+void StoneBlockClass::constructMesh(const Block* self, const BlockVTable* vTable, ChunkMesh* mesh, const Chunk* chunk, const WorldPosition& position, const glm::vec3& vertexOffset, const MappedAdjacentAndBuriedChunks& adjacentChunks)
 {
-	return Block();
+	return BlockVTable::Default::FullCubeMeshFunc(self, vTable, mesh, chunk, position, vertexOffset, adjacentChunks);
 }
 
-void StoneBlockClass::ConstructBlockVTable(BlockVTable* vTable) const
-{
-	vTable->getFaceTexture = StoneBlockClass::GetFaceTexture;
-}
-
-BlockMaterial StoneBlockClass::GetMaterial() const
-{
-	BlockMaterial material;
-	material.model = EBlockModel::Cube;
-	material.faceTextures.bottomFaceTexture = EBlockTexture::stone;
-	material.faceTextures.northFaceTexture = EBlockTexture::stone;
-	material.faceTextures.eastFaceTexture = EBlockTexture::stone;
-	material.faceTextures.southFaceTexture = EBlockTexture::stone;
-	material.faceTextures.westFaceTexture = EBlockTexture::stone;
-	material.faceTextures.topFaceTexture = EBlockTexture::stone;
-	return material;
-}
-
-EBlockTexture StoneBlockClass::GetFaceTexture(const Block* self, const BlockFacing face)
+EBlockTexture StoneBlockClass::getFaceTexture(const Block* self, const BlockFacing face)
 {
 	return EBlockTexture::stone;
 }
--- Cube/Source/World/Block/BlockClass.h
+++ Cube/Source/World/Block/BlockClass.h
@@ -1,24 +1,11 @@
 #pragma once
 
 #include "../../Engine/EngineCore.h"
-#include "BlockVTable.h"
 #include "Block.h"
 #include "../WorldTransform.h"
 #include "BlockPathtrace.h"
 
 class IBlockClass;
-
-//template<typename T>
-//concept BlockImplementsName =
-//	requires(T) {
-//		{ T::GetStaticName() } -> std::same_as<GlobalString>;
-//	};
-
-//#define BLOCK_CLASS(name)\
-//public:\
-//static GlobalString GetStaticName() {return name;}
-
-
 
 struct BlockConstructionPair
 {
@@ -29,42 +16,42 @@ struct BlockConstructionPair
 class IBlockClass {
 public:
 
-	const BlockVTable* NewBlockVTable() const;
-
-	virtual GlobalString GetBlockName() const = 0;
-	/* DO NOT set the v table. */
-	virtual Block ClassDefaultBlock() const = 0;
-
-	virtual void ConstructBlockVTable(BlockVTable* vTable) const = 0;
-
-	virtual BlockMaterial GetMaterial() const = 0;
+	virtual GlobalString getBlockName() const = 0;
+	virtual bool isFullSolid() const = 0;
 };
 
 
 class StoneBlockClass : public IBlockClass
 {
-	virtual GlobalString GetBlockName() const override { return "stone"; }
-	virtual Block ClassDefaultBlock() const override;
-	virtual void ConstructBlockVTable(BlockVTable* vTable) const override;
-	virtual BlockMaterial GetMaterial() const override;
+public:
+	virtual GlobalString getBlockName() const override { return "stone"; }
+	virtual bool isFullSolid() const override { return true; }
 
-	static EBlockTexture GetFaceTexture(const Block* self, const BlockFacing face);
+	static void constructMesh(const Block* self,
+		const BlockVTable* vTable,
+		ChunkMesh* mesh,
+		const Chunk* chunk,
+		const WorldPosition& position,
+		const glm::vec3& vertexOffset,
+		const MappedAdjacentAndBuriedChunks& adjacentChunks);
+	static EBlockTexture getFaceTexture(const Block* self, const BlockFacing face);
+
 };
 
 class AirBlockClass : public IBlockClass
 {
-	virtual GlobalString GetBlockName() const override { return "air"; }
-	virtual Block ClassDefaultBlock() const override;
-	virtual void ConstructBlockVTable(BlockVTable* vTable) const override;
-	virtual BlockMaterial GetMaterial() const override;
+public:
+	virtual GlobalString getBlockName() const override { return "air"; }
+	virtual bool isFullSolid() const override { return false; }
 
-	static EBlockTexture GetFaceTexture(const Block* self, const BlockFacing face);
-
-	static void AirNoMesh(
-		const Block* self,
-		ChunkMesh* mesh, const
-		Chunk* chunk,
-		const WorldPosition position,
-		const glm::vec3 vertexOffset,
+	static void constructMesh(const Block* self,
+		const BlockVTable* vTable,
+		ChunkMesh* mesh,
+		const Chunk* chunk,
+		const WorldPosition& position,
+		const glm::vec3& vertexOffset,
 		const MappedAdjacentAndBuriedChunks& adjacentChunks);
+	static EBlockTexture getFaceTexture(const Block* self, const BlockFacing face);
+	
+
 };
